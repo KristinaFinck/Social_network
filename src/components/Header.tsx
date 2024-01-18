5344901cafe2619db75398963923ebcb93f11f16
KristinaFinck <kristana.finck83@gmail.com>
2024-01-18 16:59:59 +0200

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className = 'header'>
+            {/*<img alt='beach' src='https://img.freepik.com/free-photo/beautiful-tropical-beach-and-sea-with-coconut-palm-tree-in-paradise-island_74190-2206.jpg?w=1800&t=st=1705572468~exp=1705573068~hmac=8cf2222e74b7558afdc6aba0023689aa4526f6c7bd8573018eb76f141daf8d40'/> */}
+            <img alt='logo' src='https://as1.ftcdn.net/v2/jpg/01/19/55/34/1000_F_119553467_rdTUkO1qTOAHZuhUernmCSTb9Du2NBn5.jpg' />
+        </header>
+    )
+}
