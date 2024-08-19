cce64c10bd94f8b258cd0b956d22ba9aaef04a46
KristinaFinck <kristina.finck83@gmail.com>
2024-08-19 17:04:54 +0300

    Implement state management with subscription and rerender logic for post addition and textarea updates. Lesson 35.

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,27 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import {addPost, state} from './redax/state'
+import {addPost, state, subscribe, updateNewPostText} from './redax/state'
+import {StateType} from "./Types";
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {App} from "./App";
 
-import {rerenderEntireTree} from "./render";
-
-
+const root = document.getElementById('root');
+export let rerenderEntireTree = (state: StateType) => {
+    ReactDOM.render(
+            <BrowserRouter>
+                <App state={state}
+                     addPost={addPost}
+                     updateNewPostText = {updateNewPostText}
+                />
+            </BrowserRouter>, root
+    );
+}
+// Подписываем функцию рендера на изменения состояния
+subscribe(rerenderEntireTree);
+// Инициализируем первое рендеринг приложения
 rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
